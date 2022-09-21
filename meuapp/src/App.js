cad521daa64b68ac661687af8b417aa56b62c79b
Neto002 <antoniogfneto92@hotmail.com>
2022-09-21 13:53:08 -0300

    utilizando componentes e states

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+// Criando componentes
+/* import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ class Equipe extends Component {
+  render() {
+    return (
+      <div>
+        <Sobre
+          nome={this.props.nome}
+          cargo={this.props.cargo}
+          idade={this.props.idade}
+        />
+        <Social fb={this.props.facebook} />
+        <hr />
+      </div>
+    );
+  }
 }
 
-export default App;
+class Sobre extends Component {
+  render() {
+    return (
+      <div>
+        <h2> Olá sou o {this.props.nome} </h2>
+        <h2> Sou {this.props.cargo} </h2>
+        <h2> Tenho {this.props.idade} anos </h2>
+      </div>
+    );
+  }
+}
+
+const Social = (props) => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href={props.fb} target="_blank" rel="noreferrer">
+            Facebook
+          </a>
+        </li>
+        <li>
+          <a href={props.fb} target="_blank" rel="noreferrer">
+            Linkedin
+          </a>
+        </li>
+        <li>
+          <a href={props.fb} target="_blank" rel="noreferrer">
+            Youtube
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <div>
+      <h1>Conheça nossa equipe</h1>
+      <Equipe
+        nome="Neto"
+        cargo="Dev"
+        idade="20"
+        facebook="https://www.google.com"
+      />
+    </div>
+  );
+} */
+
+// Trabalhando com State
+/* import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: "Neto",
+      contador: 0,
+    };
+    this.aumentar = this.aumentar.bind(this);
+    this.diminuir = this.diminuir.bind(this);
+  }
+
+  aumentar() {
+    let state = this.state;
+    state.contador++;
+    this.setState(state);
+  }
+
+  diminuir() {
+    let state = this.state;
+    if (this.state.contador > 0) {
+      state.contador--;
+      this.setState(state);
+    } else {
+      alert("Contador igual a 0");
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Contador</h1>
+        <h3>
+          <button onClick={this.diminuir}>-</button>
+          {this.state.contador}
+          <button onClick={this.aumentar}>+</button>
+        </h3>
+      </div>
+    );
+  }
+} */
