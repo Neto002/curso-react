824dc7db2420ec935523331781351090aa7bff2a
Neto002 <antoniogfneto92@hotmail.com>
2022-09-22 11:05:48 -0300

    ciclo de vida dos componentes

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -110,3 +110,32 @@ export default class App extends Component {
     );
   }
 } */
+
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hora: '00:00:00'
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ hora: new Date().toLocaleTimeString() })
+    }, 1000);
+  }
+
+  componentDidUpdate() {
+    console.log('atualizou');
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Meu Projeto {this.state.hora} </h1>
+      </div>
+    );
+  }
+}
