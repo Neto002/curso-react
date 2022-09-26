822014f58d42f22b380b49f6357661c1e61364a4
Neto002 <antoniogfneto92@hotmail.com>
2022-09-26 10:42:39 -0300

    criando e estilizando lista de filmes

--- prime-flix/src/pages/Home/index.js
+++ prime-flix/src/pages/Home/index.js
@@ -1,28 +1,42 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import api from "../../services/api";
+import './style.css'
 
 export default function Home() {
+  const [filmes, setFilmes] = useState([]);
 
-    const [filmes, setFilmes] = useState([])
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "8722a889e8aa183266cfc4a9386ffa0e",
+          language: "pt-BR",
+          page: 1,
+        },
+      });
+      setFilmes(response.data.results.slice(0, 10));
+    }
 
-    useEffect(() => {
-        async function loadFilmes() {
-            const response = await api.get("movie/now_playing", {
-                params: {
-                    api_key: '8722a889e8aa183266cfc4a9386ffa0e',
-                    language: 'pt-BR',
-                    page: 1
-                }
-            });
-            console.log(response.data.results);
-        }
+    loadFilmes();
+  }, []);
 
-        loadFilmes();
-    }, [])
-
-    return(
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+  return (
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                alt={filme.title}
+              />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
