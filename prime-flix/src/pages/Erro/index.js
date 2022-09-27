5c1356e5c2087c710ccd08acd51c390532f74b19
Neto002 <antoniogfneto92@hotmail.com>
2022-09-27 10:20:57 -0300

    criando pagina de detalhes do filme

--- prime-flix/src/pages/Erro/index.js
+++ prime-flix/src/pages/Erro/index.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+import './style.css'
+
 export default function Erro() {
     return(
-        <div>
-            <h1>404 - Not Found</h1>
+        <div className="not-found">
+            <h1>404</h1>
+            <h2>Página não encontrada</h2>
+            <Link to="/">Voltar para home</Link>
         </div>
     )
 }
--- prime-flix/src/pages/Home/index.js
+++ prime-flix/src/pages/Home/index.js
@@ -5,6 +5,7 @@ import './style.css'
 
 export default function Home() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadFilmes() {
@@ -16,10 +17,17 @@ export default function Home() {
         },
       });
       setFilmes(response.data.results.slice(0, 10));
+      setLoading(false);
     }
 
     loadFilmes();
   }, []);
+
+  if (loading) {
+    return (<div className="loading">
+        <h2>Carregando filmes...</h2>
+    </div>)
+  }
 
   return (
     <div className="container">
