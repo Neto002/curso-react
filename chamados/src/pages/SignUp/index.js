323b41b2cb41c79f16916739e44d3efd5ddfa879
Neto002 <antoniogfneto92@hotmail.com>
2022-09-29 18:02:11 -0300

    criando página de cadastro

--- chamados/src/pages/SignUp/index.js
+++ chamados/src/pages/SignUp/index.js
@@ -1,10 +1,52 @@
-function SignUp() {
+import { useState } from "react";
+import "../SignIn/signin.css";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
 
-    return (
-      <div>
-        cadastro
-      </div>
-    );
+function SignUp() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function submitForm(e) {
+    e.preventDefault();
+    alert('a')
   }
-  
-  export default SignUp;
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="logo-area">
+          <img src={logo} alt="logo" />
+        </div>
+
+        <form onSubmit={submitForm}>
+          <h1>Cadastro</h1>
+          <input
+            type="text"
+            placeholder="Digite seu nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="Digite seu e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Digite sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Cadastrar</button>
+        </form>
+
+        <Link to="/">Já possui conta? Entre aqui</Link>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
