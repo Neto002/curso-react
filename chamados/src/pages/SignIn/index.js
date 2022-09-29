6daba2c048523f7e303b092e34f8eeb053b77bd4
Neto002 <antoniogfneto92@hotmail.com>
2022-09-29 17:56:29 -0300

    criando página de login

--- chamados/src/pages/SignIn/index.js
+++ chamados/src/pages/SignIn/index.js
@@ -1,10 +1,45 @@
-function SignIn() {
+import { useState } from "react";
+import "./signin.css";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
 
-    return (
-      <div>
-        login
-      </div>
-    );
+function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function submitForm(e) {
+    e.preventDefault();
+    alert('a')
   }
-  
-  export default SignIn;
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="logo-area">
+          <img src={logo} alt="logo" />
+        </div>
+
+        <form onSubmit={submitForm}>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="Digite seu e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Digite sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link to="/register">Criar uma Conta</Link>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
