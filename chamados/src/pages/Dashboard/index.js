c7b35a39e30c7a0ca7f6bf09c77da58e8bcd34e1
Neto002 <antoniogfneto92@hotmail.com>
2022-10-06 10:09:01 -0300

    buscando chamados

--- chamados/src/pages/Dashboard/index.js
+++ chamados/src/pages/Dashboard/index.js
@@ -1,73 +1,143 @@
-import { useState } from "react"
-import { FiEdit2, FiMessageSquare, FiPlus, FiSearch } from "react-icons/fi"
-import { Link } from "react-router-dom"
-import Header from "../../components/Header"
-import Title from '../../components/Title'
-import './dashboard.css'
+import { useEffect, useState } from "react";
+import { FiEdit2, FiMessageSquare, FiPlus, FiSearch } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import firebase from "../../firebase/config";
+import Header from "../../components/Header";
+import Title from "../../components/Title";
+import "./dashboard.css";
+import { format } from "date-fns";
+
+const listRef = firebase
+  .firestore()
+  .collection("chamados")
+  .orderBy("created", "desc");
 
 export default function Dashboard() {
+  const [chamados, setChamados] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [lastDocs, setLastDocs] = useState();
 
-    const [chamados, setChamados] = useState([]);
+  useEffect(() => {
+    loadChamados();
 
-    return(
-        <div>
-            <Header/>
-            <div className="content">
-                <Title name="Atendimentos" >
-                    <FiMessageSquare size={25} />
-                </Title>
+    return () => {};
+  }, []);
 
-                {chamados.length === 0 ? (
+  async function loadChamados() {
+    await listRef
+      .limit(5)
+      .get()
+      .then((snapshot) => {
+        updateState(snapshot);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoadingMore(false);
+      });
 
-                    <div className="container dashboard">
-                    <span>Nenhum chamado registrado...</span>
+    setLoading(false);
+  }
 
-                    <Link to="/new" className="new">
-                        <FiPlus size={25} color="white"/>
-                        Novo chamado
-                    </Link>
-                    </div>
-                ) : (
-                    <>
-                        <Link to="/new" className="new">
-                            <FiPlus size={25} color="white" />
-                            Novo Chamado
-                        </Link>
+  async function updateState(snapshot) {
+    let isCollectionEmpty = snapshot.size === 0;
 
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th scope="col">Cliente</th>
-                                    <th scope="col">Assunto</th>
-                                    <th scope="col">Status</th>
-                                    <th scope="col">Cadastrado em</th>
-                                    <th scope="col">#</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td data-label="Cliente">Sujeito</td>
-                                    <td data-label="Assunto">Suporte</td>
-                                    <td data-label="Status">
-                                        <span className="badge" style={{backgroundColor: '#5cb85c'}}>Em aberto</span>
-                                    </td>
-                                    <td data-label="Cadastrado">20/06/2021</td>
-                                    <td data-label="#">
-                                        <button className="action" style={{backgroundColor: '#3583f6'}}>
-                                            <FiSearch color="white" size={17} />
-                                        </button>
+    if (!isCollectionEmpty) {
+      let lista = [];
 
-                                        <button className="action" style={{backgroundColor: '#f6a935'}}>
-                                            <FiEdit2 color="white" size={17} />
-                                        </button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </>
-                )}
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          assunto: doc.data().assunto,
+          cliente: doc.data().cliente,
+          clienteId: doc.data().clienteId,
+          created: doc.data().created,
+          createdFormated: format(doc.data().created.toDate(), "dd/MM/yyyy"),
+          status: doc.data().status,
+          complemento: doc.data().complemento,
+        });
+      });
 
-            </div>
-        </div>
-    )
+      let lastDoc = snapshot.docs[snapshot.docs.length - 1];
+
+      setChamados((chamados) => [...chamados, ...lista]);
+      setLastDocs(lastDoc);
+    } else {
+      setIsEmpty(true);
+    }
+
+    setLoadingMore(false);
+  }
+
+  return (
+    <div>
+      <Header />
+      <div className="content">
+        <Title name="Atendimentos">
+          <FiMessageSquare size={25} />
+        </Title>
+
+        {chamados.length === 0 ? (
+          <div className="container dashboard">
+            <span>Nenhum chamado registrado...</span>
+
+            <Link to="/new" className="new">
+              <FiPlus size={25} color="white" />
+              Novo chamado
+            </Link>
+          </div>
+        ) : (
+          <>
+            <Link to="/new" className="new">
+              <FiPlus size={25} color="white" />
+              Novo Chamado
+            </Link>
+
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col">Cliente</th>
+                  <th scope="col">Assunto</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Cadastrado em</th>
+                  <th scope="col">#</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td data-label="Cliente">Sujeito</td>
+                  <td data-label="Assunto">Suporte</td>
+                  <td data-label="Status">
+                    <span
+                      className="badge"
+                      style={{ backgroundColor: "#5cb85c" }}
+                    >
+                      Em aberto
+                    </span>
+                  </td>
+                  <td data-label="Cadastrado">20/06/2021</td>
+                  <td data-label="#">
+                    <button
+                      className="action"
+                      style={{ backgroundColor: "#3583f6" }}
+                    >
+                      <FiSearch color="white" size={17} />
+                    </button>
+
+                    <button
+                      className="action"
+                      style={{ backgroundColor: "#f6a935" }}
+                    >
+                      <FiEdit2 color="white" size={17} />
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
